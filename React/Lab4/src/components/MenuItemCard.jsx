bc8e6fc0590fc2106d26ad46fafd5fb2bd1b6e85
Inae Cho <inae.cho.mil@army.mil>
2026-04-06 09:25:35 -0500

    Added button group on Menu Page: quantity and add to cart.

--- React/Lab4/src/components/MenuItemCard.jsx
+++ React/Lab4/src/components/MenuItemCard.jsx
@@ -1,11 +1,41 @@
 import Card from 'react-bootstrap/Card';
 import Dropdown from 'react-bootstrap/Dropdown';
+import Button from 'react-bootstrap/Button';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import React, {useState} from 'react';
 import {menuItems} from '../data/menuData.js'
+import {ButtonGroup} from "react-bootstrap";
 
 const MenuItemCard = () => {
+
+    const [filter, setFilter] = useState('All')
+
+    const filteredItems = filter === 'All'
+        ?  menuItems
+        : menuItems.filter(item => item.category === filter)
+
     return (
         <>
-            {menuItems.map(({id, name, element, price, category}) => {
+            <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    {filter}
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    <Dropdown.Item onClick={() => setFilter('All')}>All</Dropdown.Item>
+                    <Dropdown.Item onClick={() => setFilter('Appetizer')}>Appetizer</Dropdown.Item>
+                    <Dropdown.Item onClick={() => setFilter('Lunch')}>Lunch</Dropdown.Item>
+                    <Dropdown.Item onClick={() => setFilter('Dinner')}>Dinner</Dropdown.Item>
+                    <Dropdown.Item onClick={() => setFilter('Drinks')}>Drinks</Dropdown.Item>
+
+                </Dropdown.Menu>
+            </Dropdown>
+
+            <div>
+
+            </div>
+
+            {filteredItems.map(({id, name, element, price, category}) => {
                 return (
                     <Card key={id} style={{width: '18rem'}}>
                         <Card.Body>
@@ -15,19 +45,18 @@ const MenuItemCard = () => {
                         </Card.Body>
                         <Card.Footer>
                             ${price}
-                            <Dropdown>
-                                <Dropdown.Toggle variant="success" id="dropdown-basic">
-                                    Quantity
-                                </Dropdown.Toggle>
+                            <ButtonGroup>
+                                <DropdownButton as={ButtonGroup} title="Quantity" id="bg-nested-dropdown">
+                                    <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                                    <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                                    <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                                    <Dropdown.Item eventKey="4">4</Dropdown.Item>
+                                    <Dropdown.Item eventKey="5">5</Dropdown.Item>
+                                </DropdownButton>
 
-                                <Dropdown.Menu>
-                                    <Dropdown.Item href="#/action-1">1</Dropdown.Item>
-                                    <Dropdown.Item href="#/action-2">2</Dropdown.Item>
-                                    <Dropdown.Item href="#/action-3">3</Dropdown.Item>
-                                    <Dropdown.Item href="#/action-4">4</Dropdown.Item>
-                                    <Dropdown.Item href="#/action-5">5</Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
+                                <Button>Add to Cart</Button>
+                            </ButtonGroup>
+
                         </Card.Footer>
                     </Card>
                 )
@@ -38,3 +67,18 @@ const MenuItemCard = () => {
 }
 
 export default MenuItemCard;
+
+
+// <Dropdown>
+//     <Dropdown.Toggle variant="success" id="dropdown-basic">
+//         Quantity
+//     </Dropdown.Toggle>
+//
+//     <Dropdown.Menu>
+//         <Dropdown.Item href="#/action-1">1</Dropdown.Item>
+//         <Dropdown.Item href="#/action-2">2</Dropdown.Item>
+//         <Dropdown.Item href="#/action-3">3</Dropdown.Item>
+//         <Dropdown.Item href="#/action-4">4</Dropdown.Item>
+//         <Dropdown.Item href="#/action-5">5</Dropdown.Item>
+//     </Dropdown.Menu>
+// </Dropdown>
--- React/Lab4/src/pages/MenuPage.jsx
+++ React/Lab4/src/pages/MenuPage.jsx
@@ -1,23 +1,8 @@
-import Dropdown from 'react-bootstrap/Dropdown';
 import MenuItemCard from "../components/MenuItemCard.jsx";
 
 const MenuPage = () => {
     return (
         <>
-            {/*DropDown button for Menu Categories*/}
-            <Dropdown>
-                <Dropdown.Toggle variant="success" id="dropdown-basic">
-                    Menu Category
-                </Dropdown.Toggle>
-
-                <Dropdown.Menu>
-                    <Dropdown.Item href="#/action-1">Appetizer</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">Lunch</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Dinner</Dropdown.Item>
-                    <Dropdown.Item href="#/action-4">Drinks</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
-
             <MenuItemCard/>
         </>
     )
