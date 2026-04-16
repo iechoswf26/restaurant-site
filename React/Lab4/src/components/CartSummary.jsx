01e8a069bf968e5f6060a13211cea12027586d1c
Inae Cho <inae.cho.mil@army.mil>
2026-04-15 21:07:33 -0500

    Added 3 modals to CartPage.

--- React/Lab4/src/components/CartSummary.jsx
+++ React/Lab4/src/components/CartSummary.jsx
@@ -5,11 +5,8 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-const CartSummary = ({cart}) => {
 
-    if(cart.length === 0) {
-        console.log("Your cart is empty")
-    }
+const CartSummary = ({cart}) => {
 
     const usd = new Intl.NumberFormat(
         "en-US",
@@ -22,37 +19,41 @@ const CartSummary = ({cart}) => {
     const total = subtotal + tax;
 
     return (
-        <Card>
-            <Card.Body>
-                <Card.Title>Cart</Card.Title>
-                <Container>
-                    <Row>
-                        <Col><strong>Item</strong></Col>
-                        <Col><strong>Quantity</strong></Col>
-                        <Col><strong>Price</strong></Col>
-                        <Col><strong>Line Total</strong></Col>
-                    </Row>
-                    <hr/>
-                    {cart.map((item) => (
-                        <Row key={item.id}>
-                            <Col>{item.name}</Col>
-                            <Col>{item.quantity}</Col>
-                            <Col>{usd.format(item.price)}</Col>
-                            <Col>{usd.format(item.quantity * item.price)}</Col>
+        <div>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Cart</Card.Title>
+                    <Container>
+                        <Row>
+                            <Col><strong>Item</strong></Col>
+                            <Col><strong>Quantity</strong></Col>
+                            <Col><strong>Price</strong></Col>
+                            <Col><strong>Line Total</strong></Col>
                         </Row>
-                    ))}
-                </Container>
-            </Card.Body>
-            <Card.Footer className="d-flex justify-content-center">
-                <ul>
-                    <li type="none"><strong>Subtotal: </strong>{usd.format(subtotal)}</li>
-                    <li type="none"><strong>Taxes (8%): </strong>{usd.format(tax)}</li>
-                    <li type="none"><strong>Total: </strong>{usd.format(total)}</li>
-                </ul>
-            </Card.Footer>
+                        <hr/>
 
-            <button type="submit">Checkout</button>
-        </Card>
+                        {cart.length ? cart.map((item) => (
+                            <Row key={item.id}>
+                                <Col>{item.name}</Col>
+                                <Col>{item.quantity}</Col>
+                                <Col>{usd.format(item.price)}</Col>
+                                <Col>{usd.format(item.quantity * item.price)}</Col>
+                            </Row>
+                        )): <p>Your cart is empty!</p>}
+                    </Container>
+                </Card.Body>
+                <Card.Footer className="d-flex justify-content-center">
+                    <ul>
+                        <li type="none"><strong>Subtotal: </strong>{usd.format(subtotal)}</li>
+                        <li type="none"><strong>Taxes (8%): </strong>{usd.format(tax)}</li>
+                        <li type="none"><strong>Total: </strong>{usd.format(total)}</li>
+                    </ul>
+                </Card.Footer>
+
+            </Card>
+
+        </div>
+
     )
 }
 
--- React/Lab4/src/pages/CartPage.jsx
+++ React/Lab4/src/pages/CartPage.jsx
@@ -1,13 +1,97 @@
 import { useCart } from '../context/CartContext.jsx';
+import { useState } from 'react';
 import CartSummary from "../components/CartSummary.jsx";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 const CartPage = () => {
-    const {cart} = useCart();
+    const {cart, clearCart} = useCart();
+
+
+
+
+    const [showConfirm, setShowConfirm] = useState(false);
+    const handleShowConfirm = () => setShowConfirm(true);
+    const handleCloseConfirm = () => {
+        setShowConfirm (false);
+        clearCart();
+    }
+
+    const [showCancel, setShowCancel] = useState(false);
+    const handleShowCancel = () => setShowCancel(true);
+    const handleCloseCancel = () => {
+        setShowCancel (false)
+        setShowConfirmCancel (true)
+        clearCart();
+    }
+
+    const [showConfirmCancel, setShowConfirmCancel] = useState(false);
+    const handleShowConfirmCancel = () => setShowConfirmCancel(true);
+    const handleCloseConfirmAgain = () => {
+        setShowConfirmCancel (false);
+    }
+
+
 
     return (
         <div>
 
             <CartSummary cart = {cart}/>
+
+            <br/>
+
+            <div>
+                {cart.length >= 1 && (<Button variant="primary" onClick={handleShowConfirm}>
+                    Submit Order
+                </Button>)}
+
+                <Modal show={showConfirm} onHide={handleCloseConfirm}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Order Confirmed</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Your order has been submitted!</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleCloseConfirm}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+
+            <div>
+                {cart.length >= 1 && (<Button variant="secondary" onClick={handleShowCancel}>
+                    Cancel Order
+                </Button>)}
+
+                <Modal show={showCancel}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Cancel Order?</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Are you sure you want to cancel?</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={() => {setShowCancel(false)}}>
+                            No, Keep Order
+                        </Button>
+                        <Button variant="primary" onClick={handleCloseCancel}>
+                            Yes, Cancel Order
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+
+            <div>
+                <Modal show={showConfirmCancel}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Confirm Cancel</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Thank you, come again!</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="primary" onClick={() => {setShowConfirmCancel (false)}}>
+                            Yes, Cancel Order
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
 
         </div>
 
