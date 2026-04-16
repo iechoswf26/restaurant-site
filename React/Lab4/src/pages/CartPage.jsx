a3f7b58760a38ca821b62aa707a77114eb164a0d
Inae Cho <inae.cho.mil@army.mil>
2026-04-16 16:34:12 -0500

    Fixed CartContext.

--- React/Lab4/src/pages/CartPage.jsx
+++ React/Lab4/src/pages/CartPage.jsx
@@ -5,7 +5,7 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 const CartPage = () => {
-    const {cart, clearCart} = useCart();
+    const {cart, updateQuantity, removeFromCart, totalPrice, clearCart} = useCart();
 
 
 
