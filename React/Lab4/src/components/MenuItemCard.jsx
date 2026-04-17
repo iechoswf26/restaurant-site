75ddafec1066ac8e125a023e2ff3c6116ebbbc75
Inae Cho <inae.cho.mil@army.mil>
2026-04-16 21:33:37 -0500

    Deleted comments from menuItemCard.

--- React/Lab4/src/components/MenuItemCard.jsx
+++ React/Lab4/src/components/MenuItemCard.jsx
@@ -45,24 +45,3 @@ const MenuItemCard = (item) => {
 }
 
 export default MenuItemCard;
-
-// <Card key={item.id}>
-//     {/*<Card.Img variant="top" src="" />*/}
-//     <Card.Body>
-//         <Card.Title>{item.name}</Card.Title>
-//         <Card.Subtitle className="mb-2 text-muted">{item.category}</Card.Subtitle>
-//         <Card.Text>{item.element}</Card.Text>
-//         <Card.Text>${item.price}</Card.Text>
-//     </Card.Body>
-//     <Card.Footer>
-//         <Form.Select onChange={(e) => setQuantity((e.target.value))} aria-label="quantity">
-//             <option>Quantity</option>
-//             <option value="1">1</option>
-//             <option value="2">2</option>
-//             <option value="3">3</option>
-//             <option value="4">4</option>
-//             <option value="5">5</option>
-//         </Form.Select>
-//         <Button onClick={() => addToCart(item, quantity)} variant="outline-primary">Add to Cart</Button>
-//     </Card.Footer>
-// </Card>
